0cb7665d147cf39813f42d3e8c43a25f08b76608
Arpit Tripathi <tarpit230@gmail.com>
2024-02-09 13:23:31 +0530

    App.jsx updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ import PlacePage from './pages/PlacePage';
 import BookingsPage from './pages/BookingsPage';
 import BookingPage from './pages/BookingPage';
 
-axios.defaults.baseURL = 'http://localhost:4000';
+axios.defaults.baseURL = 'https://lets-book-project-dpl.vercel.app';
 
 function App(){
     
