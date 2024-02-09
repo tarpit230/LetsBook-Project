e34ca1a8bb90d2d977a74e4a78931729e249b364
Arpit Tripathi <tarpit230@gmail.com>
2024-02-09 10:59:13 +0530

    api updated

--- api/index.js
+++ api/index.js
@@ -11,6 +11,7 @@ const imageDownloader = require('image-downloader');
 const multer = require('multer');
 const fs = require('fs');
 const path = require('path');
+const Booking = require('./models/Booking.js');
 require('dotenv').config()
 
 const bcryptSalt = bcrypt.genSaltSync(10);
@@ -18,14 +19,22 @@ const jwtSecret = 'asdfghj';
 
 app.use(express.json());
 app.use(cookieParser());
-const uploadsPath = path.resolve(__dirname, 'uploads');
-app.use('/uploads', express.static(uploadsPath));
+app.use('/uploads', express.static(__dirname+'/uploads'));
 app.use(cors({
     credentials: true,
     origin: 'http://localhost:3000',
 }));
 
 mongoose.connect(process.env.MONGO_URL);
+
+function getUserDataFromToken(req){
+    return new Promise((resolve, reject) => {
+        jwt.verify(req.cookies.token, jwtSecret, {}, async (err, userData) => {
+            if (err) throw err;
+            resolve(userData);
+        });
+    })   
+}
 
 app.get('/test', (req, res) => {
     res.json('test ok');
@@ -94,7 +103,6 @@ app.post('/upload-by-link', async (req, res) => {
         console.error('Error downloading/uploading image:', error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
-    
 })
 
 const photosMiddleware = multer({ dest: 'uploads' })
@@ -106,7 +114,7 @@ app.post('/upload', photosMiddleware.array('photos', 100), (req, res) => {
         const ext = parts[parts.length - 1];
         const newPath = path + '.' + ext;
         fs.renameSync(path, newPath);
-        uploadedFiles.push(newPath);
+        uploadedFiles.push(newPath.replace('uploads\\',''));
     }
     res.json(uploadedFiles);
 })
@@ -116,7 +124,7 @@ app.post('/places', (req, res) => {
     const {
         title, address,addedPhotos,
         description, perks, extraInfo,
-        checkIn, checkOut, maxGuests
+        checkIn, checkOut, maxGuests, price
     } = req.body;
     jwt.verify(token, jwtSecret, {}, async (err, userData) => {
         if (err) throw err;
@@ -124,7 +132,7 @@ app.post('/places', (req, res) => {
             owner:userData.id,
             title, address, addedPhotos,
             description, perks, extraInfo,
-            checkIn, checkOut, maxGuests,
+            checkIn, checkOut, maxGuests,price,
         })
         res.json(placeDoc);
     });
@@ -144,12 +152,12 @@ app.get('/places/:id', async (req, res) => {
 
 })
 
-app.put('/places/:id', async (req, res) => {
+app.put('/places', async (req, res) => {
     const { token } = req.cookies;
     const {
         id,title, address,addedPhotos,
         description, perks, extraInfo,
-        checkIn, checkOut, maxGuests
+        checkIn, checkOut, maxGuests,price,
     } = req.body;
     
     jwt.verify(token, jwtSecret, {}, async (err, userData) => {
@@ -157,9 +165,9 @@ app.put('/places/:id', async (req, res) => {
         const placeDoc = await Place.findById(id);
         if(userData.id === placeDoc.owner.toString()){
             placeDoc.set({
-            title, address, addedPhotos,
+            title, address, photos:addedPhotos,
             description, perks, extraInfo,
-            checkIn, checkOut, maxGuests,
+            checkIn, checkOut, maxGuests, price,
             })
             await placeDoc.save();
             res.json('ok');
@@ -169,6 +177,26 @@ app.put('/places/:id', async (req, res) => {
 
 app.get('/places', async (req, res) => {
     res.json(await Place.find());
+})
+
+app.post('/bookings', async (req, res) => {
+    const userData = await getUserDataFromToken(req);
+    const {place,checkIn,checkOut,numberOfGuests,name,phone,price} = req.body;
+    Booking.create({
+        place,checkIn,checkOut,numberOfGuests,name,phone,price,
+        user:userData.id,
+    }).then((doc) =>{
+        res.json(doc);
+    }).catch(err => {
+        if (err) throw err;
+    })
+})
+
+
+
+app.get('/bookings', async (req, res)  => {
+    const userData = await getUserDataFromToken(req);
+    res.json(await Booking.find({user:userData.id}).populate('place'));
 })
 
 app.listen(4000, () => {
